5d8cb5b7d5ad65f9414c43714fde57f0356c907a
chitra <chmanu.chinu@gmail.com>
2023-03-20 15:15:26 +0000

    update roboshop-infra

--- ec2/main.tf
+++ ec2/main.tf
@@ -33,8 +33,7 @@ resource "null_resource" "provisioner" {
     }
 }
 
-resource "aws_security_group" "sg"
-{
+resource "aws_security_group" "sg" {
   name        = "${var.component}-${var.env}-sg"
   description = "Allow TLS inbound traffic"
   
