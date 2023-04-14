2ad7b6363ec00b0d9b92089caf41d085196f3f69
chitra <chmanu.chinu@gmail.com>
2023-04-14 05:06:35 +0000

    update roboshop-infra

--- env-prod/locals.tf
+++ env-prod/locals.tf
@@ -1,4 +1,4 @@
 locals {
     ## private subnets
-    private_subnet_ids = [for i, v module.vpc.private_subnets : "${i} is ${v.id}"]
+    private_subnet_ids = {for i, v module.vpc.private_subnets : k => v.id }
 }
