f75ec7478f55bf5ebba1613593bd6dc1b623bc36
chitra <chmanu.chinu@gmail.com>
2023-04-14 16:20:42 +0000

    roboshop-infra

--- main.tf
+++ main.tf
@@ -40,7 +40,7 @@ module "rds" {
   engine = each.value["engine"] 
     backup_retention_period = each.value["backup_retention_period"]
   preferred_backup_window = each.value["preferred_backup_window"]
-  engine_version = each.value["engine_version"]
+  //engine_version = each.value["engine_version"]
   instance_class = each.value["instance_class"]
   no_of_instances = each.value["no_of_instances"]
   
